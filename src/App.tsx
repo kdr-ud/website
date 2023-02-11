e9e23d6a8864272007c581fa3733a1c65724f558
Joseph <joeycasa@udel.edu>
2023-02-10 19:09:33 -0500

    minor change

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ function App(): JSX.Element {
             </header>
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
-                automatically reload. Joseph Casagrande. Hello World!
+                automatically reload. Joseph Casagrande Hello World!
             </p>
         </div>
     );
