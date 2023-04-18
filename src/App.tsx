f77e004804b8f24080820030fee55b319e2bacb4
joeycasa <joeycasa@udel.edu>
2023-04-18 18:06:06 -0400

    logo fixed

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,20 @@
 import React from "react";
 import "./App.css";
-import { ChangeType } from "./components/ChangeType";
-import { RevealAnswer } from "./components/RevealAnswer";
-import { StartAttempt } from "./components/StartAttempt";
-import { TwoDice } from "./components/TwoDice";
-import { CycleHoliday } from "./components/CycleHoliday";
-import { Counter } from "./components/Counter";
-import { DoubleHalf } from "./bad-components/DoubleHalf";
-import { ColoredBox } from "./bad-components/ColoredBox";
-import { ShoveBox } from "./bad-components/ShoveBox";
-import { ChooseTeam } from "./bad-components/ChooseTeam";
-import { CheckAnswer } from "./form-components/CheckAnswer";
-import { GiveAttempts } from "./form-components/GiveAttempts";
-import { EditMode } from "./form-components/EditMode";
-import { MultipleChoiceQuestion } from "./form-components/MultipleChoiceQuestion";
-import { ChangeColor } from "./form-components/ChangeColor";
+// import { ChangeType } from "./components/ChangeType";
+// import { RevealAnswer } from "./components/RevealAnswer";
+// import { StartAttempt } from "./components/StartAttempt";
+// import { TwoDice } from "./components/TwoDice";
+// import { CycleHoliday } from "./components/CycleHoliday";
+// import { Counter } from "./components/Counter";
+// import { DoubleHalf } from "./bad-components/DoubleHalf";
+// import { ColoredBox } from "./bad-components/ColoredBox";
+// import { ShoveBox } from "./bad-components/ShoveBox";
+// import { ChooseTeam } from "./bad-components/ChooseTeam";
+// import { CheckAnswer } from "./form-components/CheckAnswer";
+// import { GiveAttempts } from "./form-components/GiveAttempts";
+// import { EditMode } from "./form-components/EditMode";
+// import { MultipleChoiceQuestion } from "./form-components/MultipleChoiceQuestion";
+// import { ChangeColor } from "./form-components/ChangeColor";
 
 function App(): JSX.Element {
     return (
